7e4ce43066064e50ef6af20037195accb4cb9491
prev1ew <warcied1@gmail.com>
2022-04-26 13:54:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+
+def create_dec_logger(path=''):
+    def logger(func):
+        def new_func(*args, **kwargs):
+            call_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            name = func.__name__
+            result = func()
+            entry = f'{call_time}: name "{name}", args "{args}", kwargs "{kwargs}, result "{result}"'
+            if path:
+                with open(path, 'a') as f:
+                    f.write(entry + '\n')
+            else:
+                print(entry)
+            return result
+        return new_func
+    return logger
+
+
+@create_dec_logger('test.txt')
+def test(*args, **kwargs):
+    print('Hi')
+    return False
+
+
+test(1, 2, c=2, d=3)
